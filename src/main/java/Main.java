b24048ace86d40cf2fd0db36b7c3196dfe71984d
Irina-Cezara <i.iacob@student.maastrichtuniversity.nl>
2026-05-15 19:31:52 +0200

    Added main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import com.cardio_generator.HealthDataSimulator;
+import com.data_management.DataStorage;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        if (args.length > 0 && args[0].equals("DataStorage")) {
+            DataStorage.main(new String[] {});
+        } else {
+            HealthDataSimulator.main(new String[] {});
+        }
+    }
+}
